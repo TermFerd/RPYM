75032094c92ad8d67a3723d837eec62d1dc7d750
TermFerd <tford2035@gmail.com>
2022-02-27 21:12:59 +0000

    nearly ready

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 function replaceWordOn() {
 	document.body.innerHTML = 
-	document.body.innerHTML.replace(/economy/gi, "rich people\'s yacht money");
+	document.body.innerHTML.replace(/economy/gi, "rich people\'s yacht money")
+	.replace(/economic/gi, "rich people\'s yacht money");
 };
 
 function onoff(){
