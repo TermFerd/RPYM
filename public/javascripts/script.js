c245f8f6c3da13b1be811a358dfe9903d3cd58c7
TermFerd <tford2035@gmail.com>
2022-02-27 21:03:49 +0000

    setup

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,14 @@
-function replace() {
+function replaceWordOn() {
 	document.body.innerHTML = 
-	document.body.innerHTML.replaceAll(/economy/gi, "rich people\'s yacht money");
+	document.body.innerHTML.replace(/economy/gi, "rich people\'s yacht money");
 };
 
+function onoff(){
+	let yachtifyOn = replaceWordOn();
+	currentvalue = document.getElementById('onoff').value;
+	if(currentvalue == "Yachtify Off"){
+	document.getElementById("onoff").value="Yachtify On";
+ 	  }else{
+    document.getElementById("onoff").value="Cannot Unsee!";
+    };
+};
