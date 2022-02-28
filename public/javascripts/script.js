3361f17ee714745279d6f4d07adf8558489808e2
TermFerd <tford2035@gmail.com>
2022-02-28 14:43:12 +0000

    stuuuuuff

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,8 @@
+
 function replaceWordOn() {
 	document.body.innerHTML = 
 	document.body.innerHTML.replace(/economy/gi, "rich people\'s yacht money")
 	.replace(/economic/gi, "rich people\'s yacht money");
 };
 
-function onoff(){
-	let yachtifyOn = replaceWordOn();
-	currentvalue = document.getElementById('onoff').value;
-	if(currentvalue == "Yachtify Off"){
-	document.getElementById("onoff").value="Yachtify On";
- 	  }else{
-    document.getElementById("onoff").value="Cannot Unsee!";
-    };
-};
+document.getElementById("myButton").addEventListener("click", replaceWordOn);
